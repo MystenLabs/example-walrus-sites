c242a9970518e325af3db6b6067f2d5ed6319f4a
Damir Shamanaev <damirka.ru@gmail.com>
2024-08-20 19:24:37 +0300

    formatter

--- flatland/mint-site/src/App.tsx
+++ flatland/mint-site/src/App.tsx
@@ -1,215 +1,200 @@
 import { ConnectButton, useCurrentAccount } from "@mysten/dapp-kit";
 import { isValidSuiObjectId, fromHEX } from "@mysten/sui.js/utils";
 import {
-  Box,
-  Container,
-  Flex,
-  Text,
-  Heading,
-  Link,
-  Em,
-  Separator,
-  Button,
-  Strong,
+    Box,
+    Container,
+    Flex,
+    Text,
+    Heading,
+    Link,
+    Em,
+    Separator,
+    Button,
+    Strong,
 } from "@radix-ui/themes";
 import { useState } from "react";
 import { ArrowRightIcon, CopyIcon } from "@radix-ui/react-icons";
 import { MintFlatlander } from "./MintFlatlander";
 import baseX from "base-x";
 
-var BASE36 = "0123456789abcdefghijklmnopqrstuvwxyz";
+const BASE36 = "0123456789abcdefghijklmnopqrstuvwxyz";
 const b36 = baseX(BASE36);
 
 function explorerLink(id: string): string {
-  return "https://suiscan.xyz/testnet/object/" + id;
+    return "https://suiscan.xyz/testnet/object/" + id;
 }
 
 function flatlanderLink(id: string): string {
-  return "https://" +
-    b36.encode(fromHEX(id.substring(2))) +
-    ".walrus.site";
+    return "https://" + b36.encode(fromHEX(id.substring(2))) + ".walrus.site";
 }
 
 function App() {
-  const currentAccount = useCurrentAccount();
-  const [flatlanderId, setFlatlander] = useState(() => {
-    const hash = window.location.hash.slice(1);
-    return isValidSuiObjectId(hash) ? hash : null;
-  });
+    const currentAccount = useCurrentAccount();
+    const [flatlanderId, setFlatlander] = useState(() => {
+        const hash = window.location.hash.slice(1);
+        return isValidSuiObjectId(hash) ? hash : null;
+    });
 
-  if (flatlanderId) {
-    console.log("flatlanderId: ", flatlanderId);
-  }
-
-  return (
-    <>
-      <Flex
-        position="sticky"
-        px="4"
-        py="2"
-        justify="between"
-        style={{
-          borderBottom: "1px solid var(--gray-a2)",
-        }}
-      >
-        <Box></Box>
-
-        <Box>
-          <ConnectButton />
-        </Box>
-      </Flex>
-      <Container size="2">
-        <Container size="1" mt="5" pt="2" px="4" style={{ minHeight: 500 }}>
-          {/* {currentAccount ? ( */}
-          <>
-            <Flex direction="column" justify="center" gap="1">
-              <Text size="6">Welcome to</Text>
-              <Heading size="9">
-                <Em>Flatland</Em>
-              </Heading>
-              <Text size="5">
-                a demo NFT project to showcase Walrus Sites
-              </Text>
+    return (
+        <>
+            <Flex
+                position="sticky"
+                px="4"
+                py="2"
+                justify="between"
+                style={{ borderBottom: "1px solid var(--gray-a2)" }}
+            >
+                <Box>
+                    <ConnectButton />
+                </Box>
             </Flex>
-            <Flex mt="9">
-              <Text>
-                <Em>Flatland</Em> is an{" "}
-                <Link href="https://en.wikipedia.org/wiki/Flatland">
-                  imaginary
-                </Link>{" "}
-                world with only three dimensions—width, length, and time—without
-                any height. Its inhabitants, the <Em>Flatlanders</Em>, have
-                evolved to live in this flat world, and thus resemble 2D
-                geometric shapes. What distinguishes them from each other is
-                only their <Em>number of sides</Em>, and their <Em>color</Em>.
-              </Text>
-            </Flex>
-            <Separator my="6" size="4" />
-            <Flex direction={{initial:"column", xs:"row"}} justify="between">
-            {/* <Grid columns={{initial:"2", md:"0"}} rows={{initial:"1", md:"2"}} gap="1" width="auto"> */}
-              <Flex direction="column">
-                <Flex>
-                  <Text>
-                    Join <Em>Flatland:</Em>
-                  </Text>
-                </Flex>
-                <Flex>
-                  {currentAccount ? (
-                    <MintFlatlander
-                      onCreated={(id) => {
-                        window.location.hash = id;
-                        setFlatlander(id);
-                      }}
-                    />
-                  ) : (
-                    <Text>
-                      <Strong>Please connect your Testnet wallet (button on the top right)</Strong>
-                    </Text>
-                  )}
-                </Flex>
-              </Flex>
-              {flatlanderId ? (
-                <>
-                  <Flex direction="column" align={{initial:"start", xs:"end"}} gap="2">
-                    <Text>
-                      Minted a new <Em>Flatlander!</Em> (
-                      {flatlanderId.substring(0, 8)}
-                      {"... "})
-                    </Text>
-
-                    <Flex direction="row" gap="2" pb="0">
-                      <Link href={flatlanderLink(flatlanderId)}>
-                        <Button variant="solid">
-                          View the Flatlander site! <ArrowRightIcon />
-                        </Button>
-                      </Link>
-                      <Box pt="2">
-                        <Button
-                          variant="ghost"
-                          onClick={() => {
-                            navigator.clipboard.writeText(
-                              flatlanderLink(flatlanderId),
-                            );
-                          }}
-                        >
-                          <CopyIcon />
-                        </Button>
-                      </Box>
+            <Container size="2">
+                <Container size="1" mt="5" pt="2" px="4" style={{ minHeight: 500 }}>
+                    <Flex direction="column" justify="center" gap="1">
+                        <Text size="6">Welcome to</Text>
+                        <Heading size="9">
+                            <Em>Flatland</Em>
+                        </Heading>
+                        <Text size="5">a demo NFT project to showcase Walrus Sites</Text>
                     </Flex>
-
-                    <Flex direction="column">
-                      <Flex direction="row" gap="3">
-                        <Link href={explorerLink(flatlanderId)}>
-                          <Button variant="outline">
-                            Explorer <ArrowRightIcon />
-                          </Button>
-                        </Link>
-                        <Box pt="2">
-                          <Button
-                            variant="ghost"
-                            onClick={() => {
-                              navigator.clipboard.writeText(
-                                explorerLink(flatlanderId),
-                              );
-                            }}
-                          >
-                            <CopyIcon />
-                          </Button>
-                        </Box>
-                      </Flex>
+                    <Flex mt="9">
+                        <Text>
+                            <Em>Flatland</Em> is an{" "}
+                            <Link href="https://en.wikipedia.org/wiki/Flatland">imaginary</Link>{" "}
+                            world with only three dimensions—width, length, and time—without any
+                            height. Its inhabitants, the <Em>Flatlanders</Em>, have evolved to live
+                            in this flat world, and thus resemble 2D geometric shapes. What
+                            distinguishes them from each other is only their{" "}
+                            <Em>number of sides</Em>, and their <Em>color</Em>.
+                        </Text>
                     </Flex>
-                  </Flex>
-                </>
-              ) : (
-                <></>
-              )}
-            {/* </Grid> */}
-            </Flex>
-            <Separator my="6" size="4" />
-            <Flex mb="5" direction="row">
-              <Box>
-                <Heading size="5" mb="4">
-                  What's cool about this?
-                </Heading>
-              </Box>
-              <Box>
-                <Text>
-                  <ul>
-                    <li>
-                      First and foremost, this site hosted on <Em>Sui</Em>, and
-                      all the resources it needs are loaded from <Em>Walrus</Em>
-                      . You can see the Sui object corresponding to this site in
-                      the{" "}
-                      <Link
-                        href={explorerLink(
-                          "0x049b6d3f34789904efcc20254400b7dca5548ee35cd7b5b145a211f85b2532fa",
+                    <Separator my="6" size="4" />
+                    <Flex direction={{ initial: "column", xs: "row" }} justify="between">
+                        <Flex direction="column">
+                            <Flex>
+                                <Text>
+                                    Join <Em>Flatland:</Em>
+                                </Text>
+                            </Flex>
+                            <Flex>
+                                {currentAccount ? (
+                                    <MintFlatlander
+                                        onCreated={(id) => {
+                                            window.location.hash = id;
+                                            setFlatlander(id);
+                                        }}
+                                    />
+                                ) : (
+                                    <Text>
+                                        <Strong>
+                                            Please connect your Testnet wallet (button on the top
+                                            right)
+                                        </Strong>
+                                    </Text>
+                                )}
+                            </Flex>
+                        </Flex>
+                        {flatlanderId ? (
+                            <Flex
+                                direction="column"
+                                align={{ initial: "start", xs: "end" }}
+                                gap="2"
+                            >
+                                <Text>
+                                    Minted a new <Em>Flatlander!</Em> (
+                                    {flatlanderId.substring(0, 8)}
+                                    {"... "})
+                                </Text>
+
+                                <Flex direction="row" gap="2" pb="0">
+                                    <Link href={flatlanderLink(flatlanderId)}>
+                                        <Button variant="solid">
+                                            View the Flatlander site! <ArrowRightIcon />
+                                        </Button>
+                                    </Link>
+                                    <Box pt="2">
+                                        <Button
+                                            variant="ghost"
+                                            onClick={() => {
+                                                navigator.clipboard.writeText(
+                                                    flatlanderLink(flatlanderId),
+                                                );
+                                            }}
+                                        >
+                                            <CopyIcon />
+                                        </Button>
+                                    </Box>
+                                </Flex>
+
+                                <Flex direction="column">
+                                    <Flex direction="row" gap="3">
+                                        <Link href={explorerLink(flatlanderId)}>
+                                            <Button variant="outline">
+                                                Explorer <ArrowRightIcon />
+                                            </Button>
+                                        </Link>
+                                        <Box pt="2">
+                                            <Button
+                                                variant="ghost"
+                                                onClick={() => {
+                                                    navigator.clipboard.writeText(
+                                                        explorerLink(flatlanderId),
+                                                    );
+                                                }}
+                                            >
+                                                <CopyIcon />
+                                            </Button>
+                                        </Box>
+                                    </Flex>
+                                </Flex>
+                            </Flex>
+                        ) : (
+                            <></>
                         )}
-                      >
-                        explorer.
-                      </Link>
-                    </li>
-                    <li>
-                      These resources are all fetched and loaded by custom
-                      software in your browser, without the need to use gateways
-                      (as similar IPFS- or Arweave-based solutions do).
-                    </li>
-                    <li>
-                      Finally, there is a <Em>unique site</Em> for each NFT,
-                      tied to its object ID. The site is styled based on the
-                      NFT's inner fields (in this case, the color and shape).
-                      This feature could be great to create new experiences,
-                      where the user can view and interact with the NFT not only
-                      through the wallet, but also through a dedicated site.
-                    </li>
-                  </ul>
-                </Text>
-              </Box>
-            </Flex>
-          </>
-        </Container>
-      </Container>
-    </>
-  );
+                    </Flex>
+                    <Separator my="6" size="4" />
+                    <Flex mb="5" direction="row">
+                        <Box>
+                            <Heading size="5" mb="4">
+                                What's cool about this?
+                            </Heading>
+                        </Box>
+                        <Box>
+                            <Text>
+                                <ul>
+                                    <li>
+                                        First and foremost, this site hosted on <Em>Sui</Em>, and
+                                        all the resources it needs are loaded from <Em>Walrus</Em>.
+                                        You can see the Sui object corresponding to this site in the{" "}
+                                        <Link
+                                            href={explorerLink(
+                                                "0x049b6d3f34789904efcc20254400b7dca5548ee35cd7b5b145a211f85b2532fa",
+                                            )}
+                                        >
+                                            explorer.
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        These resources are all fetched and loaded by custom
+                                        software in your browser, without the need to use gateways
+                                        (as similar IPFS- or Arweave-based solutions do).
+                                    </li>
+                                    <li>
+                                        Finally, there is a <Em>unique site</Em> for each NFT, tied
+                                        to its object ID. The site is styled based on the NFT's
+                                        inner fields (in this case, the color and shape). This
+                                        feature could be great to create new experiences, where the
+                                        user can view and interact with the NFT not only through the
+                                        wallet, but also through a dedicated site.
+                                    </li>
+                                </ul>
+                            </Text>
+                        </Box>
+                    </Flex>
+                </Container>
+            </Container>
+        </>
+    );
 }
 
 export default App;
--- flatland/nft-viz/src/index.ts
+++ flatland/nft-viz/src/index.ts
@@ -1,212 +1,198 @@
-import {
-  getFullnodeUrl,
-  SuiClient,
-  SuiObjectData,
-} from "@mysten/sui.js/client";
+import { SuiClient } from "@mysten/sui.js/client";
 import { bcs } from "@mysten/sui.js/bcs";
 import {
-  isValidSuiObjectId,
-  isValidSuiAddress,
-  fromB64,
-  fromHEX,
-  toHEX,
+    isValidSuiObjectId,
+    isValidSuiAddress,
+    fromB64,
+    fromHEX,
+    toHEX,
 } from "@mysten/sui.js/utils";
 import * as baseX from "base-x";
 
 const FLATLAND_PACKAGE = "0x55d3cec3a5f131e515f8b66b0111490b74eb4274645e61b6bc34915f6abe7f63";
-var BASE36 = "0123456789abcdefghijklmnopqrstuvwxyz";
+const BASE36 = "0123456789abcdefghijklmnopqrstuvwxyz";
 const b36 = baseX(BASE36);
 
 // Object definitions that mirror the on chain ones
 type Color = {
-  r: number;
-  g: number;
-  b: number;
+    r: number;
+    g: number;
+    b: number;
 };
 
 type Flatlander = {
-  id: string;
-  color: Color;
-  sides: number;
+    id: string;
+    color: Color;
+    sides: number;
 };
 
 // Define UID as a 32-byte array, then add a transform to/from hex strings.
 const UID = bcs.fixedArray(32, bcs.u8()).transform({
-  input: (id: string) => fromHEX(id),
-  output: (id) => toHEX(Uint8Array.from(id)),
+    input: (id: string) => fromHEX(id),
+    output: (id) => toHEX(Uint8Array.from(id)),
 });
 
 const ColorStruct = bcs.struct("Color", {
-  r: bcs.u8(),
-  g: bcs.u8(),
-  b: bcs.u8(),
+    r: bcs.u8(),
+    g: bcs.u8(),
+    b: bcs.u8(),
 });
 
 const FlatlanderStruct = bcs.struct("Flatlander", {
-  id: UID,
-  color: ColorStruct,
-  sides: bcs.u8(),
+    id: UID,
+    color: ColorStruct,
+    sides: bcs.u8(),
 });
 
-export async function getFlatlander(
-  objectId: string
-): Promise<Flatlander | null> {
-  console.log("getting the flatlander");
-  const client = new SuiClient({ url: "https://fullnode.testnet.sui.io:443" });
-  let obj = await client.getObject({
-    id: objectId,
-    options: { showBcs: true, showType: true },
-  });
-  console.log("flatlander object:", obj);
-  if (obj.data && obj.data.bcs && obj.data.bcs.dataType === "moveObject" && obj.data.type === flatlanderType()) {
-    let struct = FlatlanderStruct.parse(fromB64(obj.data.bcs.bcsBytes));
-    console.log("struct", struct);
-    return struct as Flatlander;
-  }
-  return null;
+export async function getFlatlander(objectId: string): Promise<Flatlander | null> {
+    const client = new SuiClient({ url: "https://fullnode.testnet.sui.io:443" });
+    const obj = await client.getObject({
+        id: objectId,
+        options: { showBcs: true, showType: true },
+    });
+
+    console.log("flatlander object:", obj);
+
+    if (
+        obj.data &&
+        obj.data.bcs &&
+        obj.data.bcs.dataType === "moveObject" &&
+        obj.data.type === flatlanderType()
+    ) {
+        let struct = FlatlanderStruct.parse(fromB64(obj.data.bcs.bcsBytes));
+        console.log("struct", struct);
+        return struct as Flatlander;
+    }
+
+    return null;
 }
 
 function flatlanderType(): string {
-  return FLATLAND_PACKAGE + "::flatland::Flatlander"
+    return FLATLAND_PACKAGE + "::flatland::Flatlander";
 }
 
 export async function drawFlatlander(flatlander: Flatlander) {
-  console.log("drawing the flatlander");
-  // Draw the flatlander
-  const radius = 50;
-  const centerX = 100;
-  const centerY = 100;
-  const sides = flatlander.sides;
-  const angle = (Math.PI * 2) / sides;
-  const offset = -Math.PI / 2;
-  const color = colorString(flatlander.color);
+    const radius = 50;
+    const centerX = 100;
+    const centerY = 100;
+    const sides = flatlander.sides;
+    const angle = (Math.PI * 2) / sides;
+    const offset = -Math.PI / 2;
+    const color = colorString(flatlander.color);
 
-  // Change the css color varialbe.
-  document.documentElement.style.setProperty("--color", `${color}`);
+    // Change the css color varialbe.
+    document.documentElement.style.setProperty("--color", `${color}`);
 
-  // Draw the polygon.
-  const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  svg.setAttribute("width", "200");
-  svg.setAttribute("height", "200");
-  svg.setAttribute("id", "flatland");
-  svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+    // Draw the polygon.
+    const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+    svg.setAttribute("width", "200");
+    svg.setAttribute("height", "200");
+    svg.setAttribute("id", "flatland");
+    svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
 
-  // add to the picture div
-  document.getElementById("picture").appendChild(svg);
+    // add to the picture div
+    document.getElementById("picture").appendChild(svg);
 
-  const polygon = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "polygon"
-  );
-  polygon.setAttribute("fill", `rgb(${color})`);
-  let points = "";
-  for (let i = 0; i < sides; i++) {
-    points += `${centerX + radius * Math.cos(angle * i + offset)},${
-      centerY + radius * Math.sin(angle * i + offset)
-    } `;
-  }
-  polygon.setAttribute("points", points);
+    const polygon = document.createElementNS("http://www.w3.org/2000/svg", "polygon");
+    polygon.setAttribute("fill", `rgb(${color})`);
+    let points = "";
+    for (let i = 0; i < sides; i++) {
+        points += `${centerX + radius * Math.cos(angle * i + offset)},${
+            centerY + radius * Math.sin(angle * i + offset)
+        } `;
+    }
+    polygon.setAttribute("points", points);
 
-  const animation = document.createElementNS(
-    "http://www.w3.org/2000/svg",
-    "animateTransform"
-  );
-  animation.setAttribute("attributeName", "transform");
-  animation.setAttribute("begin", "0s");
-  animation.setAttribute("dur", "10s");
-  animation.setAttribute("type", "rotate");
-  animation.setAttribute("from", "0 100 100");
-  animation.setAttribute("to", "360 100 100");
-  animation.setAttribute("repeatCount", "indefinite");
+    const animation = document.createElementNS("http://www.w3.org/2000/svg", "animateTransform");
+    animation.setAttribute("attributeName", "transform");
+    animation.setAttribute("begin", "0s");
+    animation.setAttribute("dur", "10s");
+    animation.setAttribute("type", "rotate");
+    animation.setAttribute("from", "0 100 100");
+    animation.setAttribute("to", "360 100 100");
+    animation.setAttribute("repeatCount", "indefinite");
 
-  polygon.appendChild(animation);
-  svg.appendChild(polygon);
-
-  console.log("done");
+    polygon.appendChild(animation);
+    svg.appendChild(polygon);
 }
 
 function colorString(color: Color): string {
-  return `${color.r}, ${color.g}, ${color.b}`;
+    return `${color.r}, ${color.g}, ${color.b}`;
 }
 
 type Path = {
-  subdomain: string;
-  path: string;
+    subdomain: string;
+    path: string;
 };
 
 function getSubdomainAndPath(scope: string): Path | null {
-  // At the moment we only support one subdomain level.
-  const url = new URL(scope);
-  const hostname = url.hostname.split(".");
+    // At the moment we only support one subdomain level.
+    const url = new URL(scope);
+    const hostname = url.hostname.split(".");
 
-  if (
-    hostname.length === 3 ||
-    (hostname.length === 2 && hostname[1] === "localhost")
-  ) {
-    // Accept only one level of subdomain eg `subdomain.example.com` or `subdomain.localhost` in
-    // case of local development
-    const path =
-      url.pathname == "/" ? "/index.html" : removeLastSlash(url.pathname);
-    return { subdomain: hostname[0], path } as Path;
-  }
-  return null;
+    if (hostname.length === 3 || (hostname.length === 2 && hostname[1] === "localhost")) {
+        // Accept only one level of subdomain eg `subdomain.example.com` or `subdomain.localhost` in
+        // case of local development
+        const path = url.pathname == "/" ? "/index.html" : removeLastSlash(url.pathname);
+        return { subdomain: hostname[0], path } as Path;
+    }
+    return null;
 }
 
 /** Remove the last forward-slash if present
  * Resources on chain are only stored as `/path/to/resource.extension`.
  */
 function removeLastSlash(path: string): string {
-  return path.endsWith("/") ? path.slice(0, -1) : path;
+    return path.endsWith("/") ? path.slice(0, -1) : path;
 }
 
 function subdomainToObjectId(subdomain: string): string | null {
-  const objectId = "0x" + toHEX(b36.decode(subdomain.toLowerCase()));
-  console.log(
-    "obtained object id: ",
-    objectId,
-    isValidSuiObjectId(objectId),
-    isValidSuiAddress(objectId)
-  );
-  return isValidSuiObjectId(objectId) ? objectId : null;
+    const objectId = "0x" + toHEX(b36.decode(subdomain.toLowerCase()));
+    console.log(
+        "obtained object id: ",
+        objectId,
+        isValidSuiObjectId(objectId),
+        isValidSuiAddress(objectId)
+    );
+    return isValidSuiObjectId(objectId) ? objectId : null;
 }
 
 function notFound() {
-  // Display a not found message.
-  const notFound = document.createElement("h2");
-  notFound.innerText = "sorry, not a valid flatlander";
-  document.getElementById("picture").appendChild(notFound);
+    // Display a not found message.
+    const notFound = document.createElement("h2");
+    notFound.innerText = "sorry, not a valid flatlander";
+    document.getElementById("picture").appendChild(notFound);
 }
 
-function main() {
-  // Get the origin url of the page
-  const url = window.location.origin;
-  const parsedUrl = getSubdomainAndPath(url);
-  if (!parsedUrl) {
-    notFound();
-    return;
-  }
-
-  const objectId = subdomainToObjectId(parsedUrl.subdomain);
-  if (!objectId) {
-    notFound();
-    return;
-  }
-
-  getFlatlander(objectId).then((flatlander) => {
-    if (!flatlander) {
-      notFound();
-      return;
+(function main() {
+    // Get the origin url of the page
+    const url = window.location.origin;
+    const parsedUrl = getSubdomainAndPath(url);
+    if (!parsedUrl) {
+        notFound();
+        return;
     }
-    console.log("flatlander: ", flatlander);
-    drawFlatlander(flatlander);
-    const title = document.createElement("h1");
-    title.innerText = "your flatlander";
-    document.getElementById("text").appendChild(title);
 
-    const line1 = document.createElement("p");
-    line1.innerHTML = "the color and the number of sides is taken from the NFT;</br>this URL is unique to this Flatlander, and always will be.";
-    document.getElementById("text").appendChild(line1);
-  });
-}
+    const objectId = subdomainToObjectId(parsedUrl.subdomain);
+    if (!objectId) {
+        notFound();
+        return;
+    }
 
-main();
+    getFlatlander(objectId).then((flatlander) => {
+        if (!flatlander) {
+            notFound();
+            return;
+        }
+        console.log("flatlander: ", flatlander);
+        drawFlatlander(flatlander);
+        const title = document.createElement("h1");
+        title.innerText = "your flatlander";
+        document.getElementById("text").appendChild(title);
+
+        const line1 = document.createElement("p");
+        line1.innerHTML =
+            "the color and the number of sides is taken from the NFT;</br>this URL is unique to this Flatlander, and always will be.";
+        document.getElementById("text").appendChild(line1);
+    });
+})();
