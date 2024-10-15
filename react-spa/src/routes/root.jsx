4f59bbeba21b720c78a46fdf765295506f6a5008
Alexandros Tzimas <alex.tzimas@mystenlabs.com>
2024-10-15 18:19:18 +0300

    Replace react router with SPA title bottom left

--- react-spa/src/routes/root.jsx
+++ react-spa/src/routes/root.jsx
@@ -4,7 +4,7 @@ export default function Root() {
   return (
     <>
       <div id="sidebar">
-        <h1>React Router Contacts</h1>
+        <h1>Single Page Application Example</h1>
         <div>
           <form id="search-form" role="search">
             <input
